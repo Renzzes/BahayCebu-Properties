9b3a23e79aad72879599f723910158d9c5c5115d
clare <clare@RENZ>
2025-07-28 09:22:32 +0800

    latest

--- src/main.tsx
+++ src/main.tsx
@@ -7,7 +7,7 @@ import { GoogleOAuthProvider } from '@react-oauth/google'
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <GoogleOAuthProvider 
-      clientId="897144997266-c4nm53c10808l9oj00t282p7iq3ogesp.apps.googleusercontent.com"
+      clientId={import.meta.env.VITE_GOOGLE_CLIENT_ID}
     >
       <App />
     </GoogleOAuthProvider>
